116d3a1378df37af3917e8aea5965f71fbd796d2
cesarOchoa90 <hire.cesar@outlook.com>
2025-01-24 12:57:06 -0600

    changes in authentication for login/out  nav bar

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import "./App.css";
 import Nav from "./Components/Nav";
 import Router from "./Router";
 import React, {} from "react";
+import { useAuth0 } from '@auth0/auth0-react';
 
 function App() {
+  const {isAuthenticated} = useAuth0();
   return (
     <div className="App">
-      <Nav loggedIn={true} />
+      <Nav loggedIn={isAuthenticated} />
      <Router/>
     </div>
   );
